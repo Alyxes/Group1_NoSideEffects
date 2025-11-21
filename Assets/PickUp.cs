046279d0651d4a63cda4791f35fc85bf71d5796a
Bexbornleo <leo.bexborn@gritgames.se>
2025-11-21 12:00:50 +0100

    Working pickup and drop system
    not sure if its working with different items

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -1,16 +1,74 @@
+using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    [SerializeField] public bool isCollected = false;
-    public void OnTriggerEnter(Collider other)
+    public bool isCollected = false;
+    public bool inZone = false;
+    public GameObject HoldPoint;
+
+    private void Start()
     {
-        if (other.CompareTag("Player"))
+        HoldPoint.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (inZone && Input.GetKeyDown(KeyCode.E))
         {
-            isCollected = true;
-            Debug.Log("Item picked up!");
-            Destroy(gameObject);
+            if (!isCollected)
+            {
+                PickUpItem();
+            }
+            else
+            {
+                DropItem();
+            }
         }
     }
 
+    private void PickUpItem()
+    {
+        if (GameObject.FindWithTag("Item1") == null)
+        {
+            isCollected = true;
+            transform.GetChild(0).gameObject.SetActive(false); // hide the item model
+            HoldPoint.SetActive(true); // show it at the hold point
+            gameObject.tag = "Item1";
+            Debug.Log("Item picked up");
+        }
+        else
+        {
+            Debug.Log("You are already holding an item!");
+        }
+    }
+
+    private void DropItem()
+    {
+        isCollected = false;
+        transform.GetChild(0).gameObject.SetActive(true); // show the item model again
+        HoldPoint.SetActive(false);
+        gameObject.tag = "Untagged";
+        Debug.Log("Item dropped");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inZone = true;
+            Debug.Log("Press E to pick up or drop item");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inZone = false;
+            Debug.Log("Left pickup zone");
+        }
+    }
 }
+
