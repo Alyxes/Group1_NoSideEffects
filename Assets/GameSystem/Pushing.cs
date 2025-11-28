8918ff9214d03dbcffc5c282fb969d31c1b4e756
isachilje <isac.hiljeborn@gritgames.se>
2025-11-28 13:40:05 +0100

    har fortsatt med knuffning av möbler

--- Assets/GameSystem/Pushing.cs
+++ Assets/GameSystem/Pushing.cs
@@ -6,27 +6,64 @@ namespace NoSideEffects
     public class Pushing : MonoBehaviour
     {
         private InputAction interactButton;
-        public GameObject testObject;
+        private Collider triggerCollider;
+        private bool inZone = false;
 
         private void DisableKinematic()
         {
-            testObject.GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().isKinematic = false;
         }
         private void EnableKinematic()
         {
-            testObject.GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().isKinematic = true;
         }
 
 
         void Start()
         {
-            
+            triggerCollider = GetComponent<Collider>();
+            interactButton = InputSystem.actions.FindAction("Interact");
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (interactButton.WasPressedThisFrame())
+            {
+                if (GetComponent<Rigidbody>().isKinematic == true)
+                {
+                    DisableKinematic();
+                }
+                else if(GetComponent<Rigidbody>().isKinematic == false)
+                {
+                    EnableKinematic();
+                }
+                    Debug.Log("button pressed");
+                
+            }
+        }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("PushZone"))
+            {
+                if (!inZone)
+                {
+                    inZone = true;
+                    Debug.Log("Is in pushing zone");
+                }
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("PushZone"))
+            {
+                if (inZone)
+                {
+                    inZone = false;
+                    Debug.Log("left the pushing zone");
+                }
+            }
         }
     }
 }
